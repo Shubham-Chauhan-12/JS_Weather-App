a3231e3b3caaa20678d0d1535d70d7c80fc4a787
Shubham Chauhan <shubhamchauhan1204.99@gmail.com>
2023-10-04 00:41:19 +0530

    variable declared and html css modified

---
+++ main.js
@@ -0,0 +1,7 @@
+const inputBox = document.querySelector('.input-box');
+const weatherimg = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description= document.querySelector('.description');
+const humidity= document.querySelector('.humidity');
+const windspeed= document.querySelector('.wind-speed');
+const searchBtn = document.getElementById('searchBtn');
