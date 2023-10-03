9b0d2c0fe2a914912a99e820ed58e98851bc03dc
Shubham Chauhan <shubhamchauhan1204.99@gmail.com>
2023-10-04 00:56:58 +0530

    getWeatherInfo added and got the data

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ const description= document.querySelector('.description');
 const humidity= document.querySelector('.humidity');
 const windspeed= document.querySelector('.wind-speed');
 const searchBtn = document.getElementById('searchBtn');
+
+
+
+async function getWeatherInfo(city){
+
+    const api= "e700bc3081b1f8b93b22eef9d51de646";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`;
+
+    const data = await fetch(`${url}`).then(response => response.json());
+
+    console.log(data);
+
+}
+
+
+searchBtn.addEventListener('click',()=>{
+    getWeatherInfo(inputBox.value);
+})
+
